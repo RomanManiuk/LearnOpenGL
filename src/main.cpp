013685b69f3cceb5db93523e45b3b656f86d08a7
Roman Maniuk <93196537+STARR2119@users.noreply.github.com>
2023-03-14 11:09:25 +0200

    added some code (for testing libs)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+GLuint WINDOW_WIDTH = 900;
+GLuint WINDOW_HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *pWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(pWindow);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glClearColor(0.0, 0.0, 0.5, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
